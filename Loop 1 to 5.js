f26a044378d5ff036b31c44c34dddf02a8c96a23
Vipul singh <73470976+vipullsingh@users.noreply.github.com>
2022-11-21 15:56:07 +0530

    Create Loop 1 to 5.js

---
+++ Loop 1 to 5.js
@@ -0,0 +1,39 @@
+// Loop 1 to 5 -32:4:48
+// Description
+
+// You have to print all the numbers from 1 to5
+
+// Print each number on a new line
+
+
+// Input
+// No Input
+
+
+// Output
+// Print all the numbers from 1 to5, on a new line
+
+
+// Sample Input 1 
+
+// No Input
+// Sample Output 1
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// Hint
+
+// Print all the values from1 to 5are printed on a new line
+
+function loop1to5() {
+    let i = 1
+    while(i<=5)
+    {
+        console.log(i)
+        i++
+    }
+    
+}
