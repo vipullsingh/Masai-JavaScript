7d6edd8ee886f3470e2ec2ba5de6143de7640477
Vipul singh <73470976+vipullsingh@users.noreply.github.com>
2022-11-21 15:52:38 +0530

    Create PrintMasaiNumTimes.js

---
+++ PrintMasaiNumTimes.js
@@ -0,0 +1,48 @@
+// Print Masai School II -32:8:34
+// Description
+
+// You are given a number stored in a variable with the namenum
+
+// You have to printMasai School, as many times as the number stored in the variablenum
+
+// Every time, printMasai Schoolon a new line
+
+
+// Input
+// The first and the only line of input contains a number stored in the variablenum
+
+
+// Output
+// PrintMasai School, as many times as the number stored in the variablenum, every time on a new line
+
+
+// Sample Input 1 
+
+// 5
+// Sample Output 1
+
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+// Hint
+
+// In the sample test case, the value stored innum = 5
+
+// Therefore, the output is as follows
+
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+
+function printMasaiSchool(num) {
+    while(num>0)
+    {
+        console.log("Masai School")
+        num--
+    }
+  
+}
